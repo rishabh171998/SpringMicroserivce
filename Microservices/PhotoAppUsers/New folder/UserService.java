d6471f09f71c270438de98c76c410b793088fcb2
Rishabh Bhanot <rishabhbhanot10@gmail.com>
2021-06-24 01:16:45 +0530

    Microservices added

--- Microservices/PhotoAppUsers/New folder/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.appdev.photoapp.api.users.PhotoAppUsers.services;
-
-import com.appdev.photoapp.api.users.PhotoAppUsers.shared.UserDto;
-
-public interface UserService {
-    public UserDto createUser(UserDto userDetails);
-}
--- Microservices/PhotoAppUsers/New folder/data/userRepository.java
+++
@@ -1,8 +0,0 @@
-package com.appdev.photoapp.api.users.PhotoAppUsers.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userRepository extends CrudRepository<userEntity,Long> {
-}
